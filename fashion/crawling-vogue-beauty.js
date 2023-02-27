0af8255d45fbf1fd91abea1fb4e849484a234c6e
ybkim-dev <beanybigggy@gmail.com>
2023-02-27 15:58:34 +0900

    fix : Vogue 사이트 형식변경으로 인한 크롤링 코드 변경

--- fashion/crawling-vogue-beauty.js
+++ fashion/crawling-vogue-beauty.js
@@ -1,27 +1,38 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+const utils = require('../utils.js');
+const amqp = require('amqplib');
+
+const MQ_URL = 'amqp://guest:guest@127.0.0.1';
 
 async function main() {
     resultList = []
     for(let pageNum = 1; pageNum <= 10; pageNum++) {
-        const response = await axios.get(
-            'https://www.vogue.co.kr/category/beauty/page/' + pageNum
-        );
-    
-        const $ = cheerio.load(response.data);
-        const elements = $('.fusion-posts-container.fusion-blog-layout-grid.fusion-blog-layout-grid-3.isotope.fusion-blog-layout-masonry.fusion-posts-container-infinite').children(".fusion-post-masonry.fusion-post-grid.fusion-element-portrait.post.fusion-clearfix.type-post");
-    
-        elements.each((idx, elem) => {
-            // if(Date.parse($(elem).find("span.post-meta").find("span.post-date").text()) < Date.parse("2022-01-01")) return false;
-            // console.log($(elem).find("span.post-thumb").attr("style")== undefined);
-            // console.log(new Date(Date.parse($(elem).find("span.post-meta").find("span.post-date").text())).toDateString());
+        const response = await fetch("https://www.vogue.co.kr/wp-admin/admin-ajax.php", {
+            "headers": {
+              "accept": "application/json, text/javascript, */*; q=0.01",
+              "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+              "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+              "sec-fetch-mode": "cors",
+              "sec-fetch-site": "same-origin",
+              "x-requested-with": "XMLHttpRequest"
+            },
+            "referrer": "https://www.vogue.co.kr/beauty/beauty-trend/",
+            "referrerPolicy": "strict-origin-when-cross-origin",
+            "body": "action=get_posts_custom_v2&posts_per_page=12&post_type=post&paged=" + pageNum + "&tax1_slug=beauty&tax2_slug=beauty-trend&term2_id=47",
+            "method": "POST",
+            "mode": "cors",
+            "credentials": "include"
+          });
+        
+        elements = await response.json();
+        elements = elements.current_posts;
+        console.log(elements.length);
+        elements.forEach((elem) => {
             resultList.push({
-                title: $(elem).find(".fusion-post-content-wrapper").find(".entry-title a").text(),
-                link: $(elem).find(".fusion-post-content-wrapper").find(".entry-title a").attr("href"),
+                title: elem.post_title,
+                link: elem.permalink,
                 contentImgLink: 
-                    $(elem).find(".fusion-masonry-element-container.fusion-image-wrapper a img").attr("src") != undefined  ? 
-                    $(elem).find(".fusion-masonry-element-container.fusion-image-wrapper a img").attr("src") : 
-                    "https://hyperlink-data.s3.ap-northeast-2.amazonaws.com/content-default-image/logo_vogue.png",
+                    elem.post_thumbnail_url != undefined  ? 
+                    elem.post_thumbnail_url : "https://hyperlink-data.s3.ap-northeast-2.amazonaws.com/content-default-image/logo_vogue.png",
                 category: "fashion",
                 creator: "VOGUE 패션 매거진"
             })
@@ -31,4 +42,11 @@ async function main() {
     return resultList;
 }
 main()
-.then(response => console.log(response));
+.then(async responses => {
+    console.log(responses.length);
+    const connect = await amqp.connect(MQ_URL);
+
+    await utils.connectToChannelAndPublish(connect, responses);
+
+    await connect.close();
+});
