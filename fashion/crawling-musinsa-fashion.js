999032da9dc58e9e50f310f31166bd2c8d3d4447
ybkim-dev <beanybigggy@gmail.com>
2023-03-13 14:32:25 +0900

    feat : 무신사 크리에이터 이름 변경

--- fashion/crawling-musinsa-fashion.js
+++ fashion/crawling-musinsa-fashion.js
@@ -23,7 +23,7 @@ async function main() {
         link: elem.children('a').attr('href'),
         contentImgLink: elem.find('a span.imgBox img').attr('src') != undefined ? elem.find('a span.imgBox img').attr('src') : 'https://hyperlink-data.s3.ap-northeast-2.amazonaws.com/creator-logo-image/logo_musinsa_creator.png',
         categoryName: 'beauty',
-        creatorName: "무신사"
+        creatorName: "무신사 패션 매진"
       });
     });
   }
